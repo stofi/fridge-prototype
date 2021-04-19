2e8c931a12176f38997556cec47f2b15d6bc8c04
Krystof Rainisch <krystof.rainisch@hey.com>
2021-04-19 11:25:42 +0200

    remove list of group members from space item

--- src/components/Space/Space.vue
+++ src/components/Space/Space.vue
@@ -12,18 +12,6 @@
         <p class="text-sm font-medium text-brand truncate">
           {{ group.name }}
         </p>
-        <p class="flex items-center text-sm text-gray-500">
-          <UsersIcon
-            class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          <span
-            class="bg-gray-100 dark:bg-gray-900 px-2 rounded-md mx-1"
-            v-for="user in members"
-            :key="user._id"
-            >{{ user.email }}</span
-          >
-        </p>
       </div>
     </div>
     <div class="col-span-1 flex justify-end">
