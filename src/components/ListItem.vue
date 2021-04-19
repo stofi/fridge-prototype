ae8869b092b0b12b648720d81e31bd1fbb861226
Krystof Rainisch <krystof.rainisch@hey.com>
2021-04-19 18:42:14 +0200

    fix spacing

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -2,10 +2,10 @@
   <div
     class="grid grid-cols-1 md:grid-cols-12 hover:bg-gray-50 dark:hover:bg-gray-700 px-4 py-4 sm:px-6"
   >
-    <div class="col-span-11 flex items-center justify-between">
+    <div class="col-span-11 flex items-center justify-between pt-2 sm:pt-0">
       <slot />
     </div>
-    <div class="col-span-1 mt-2 md:mt-0 flex justify-end">
+    <div class="col-span-1 mb-2 sm:mb-0 flex justify-end">
       <button
         @click="$emit('remove')"
         class="inline-flex items-center text-gray-500 focus:outline-none focus:text-red-500"
