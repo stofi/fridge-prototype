6510ba1530001e72fa9d087d3ee113d48eadbea9
Krystof Rainisch <krystof.rainisch@hey.com>
2021-04-19 11:24:55 +0200

    add navigation active state

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,12 +17,12 @@
               v-for="item in pages"
               :key="item.name"
               :to="item.path"
-              :class="[
-                item.current
-                  ? 'border-brand text-gray-900'
-                  : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium',
-              ]"
+              :class="
+                item.path == route.path
+                  ? 'border-brand text-gray-900 dark:text-gray-300'
+                  : 'border-transparent text-gray-500  hover:border-gray-300 hover:text-gray-700 dark:hover:text-gray-300'
+              "
+              class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               :aria-current="item.current ? 'page' : undefined"
             >
               {{ item.name }}
